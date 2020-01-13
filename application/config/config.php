3ac6e151220b9b1e7bebde1a0a50ee9290d363e7
unique211 <vishal.rkcet@gmail.com>
2020-01-13 15:05:11 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- $config['base_url'] = 'https://hotelinvoice.herokuapp.com/';
+ $config['base_url'] = 'http://hotelinvoice.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File

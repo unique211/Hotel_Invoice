54a7ee2690bd4225774212153d5e06cdc2e29573
Sagar Morvadiya <morvadiyasagar99@gmail.com>
2020-01-30 12:18:59 +0530

    change Title

--- application/views/bill_invoice3.php
+++ application/views/bill_invoice3.php
@@ -4,7 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-    <title>Express Wash Customer Invoice</title>
+    <title>Customer Invoice</title>
     <style>
         @media print {
             @page {
